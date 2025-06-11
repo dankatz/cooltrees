125acdf85478b9c3f2653c5c66daa6abd1b09b00
dankatz <dankatz512@gmail.com>
2025-06-11 17:19:16 -0400

    including map and animation

--- NYC_tree_flow.R
+++ NYC_tree_flow.R
@@ -6,10 +6,32 @@ library(tidyr)
 library(lubridate)
 library(ggplot2)
 library(readr)
+library(sf)
+library(ggspatial)
+library(terra)
+library(tidyterra)
+library(gifski)
 
-#loading in the start of season data from Dave's PlanetScope analysis
+
+### PlanetScope-derived start of spring ###########################################################
+# loading in the start of season data from Dave's PlanetScope analysis
 nyc_sos <- read_csv ("C:/Users/dsk273/Box/Katz lab/NYC/tree_pheno/tree_pheno_pointextract_polyid_all_output_2017_2024.csv")
 #names(nyc_sos)
+
+## QA/QC for SOS dates
+nyc_sos_percentile_sp_yr <- nyc_sos %>%     
+  filter(R2 > 0.8 & dbh > 3.9) %>% 
+  filter(n_SOS_50_7day > 0) %>% 
+  group_by(genus, species, Year) %>%
+  summarize(# nobs = n(),
+            # SOS_min = min(SOS_50),
+            SOS_p2.5 = quantile(SOS_50, 0.025),
+            # SOS_p25 = quantile(SOS_50, 0.25),
+            # SOS_p50 = quantile(SOS_50, 0.50),
+            # SOS_p75 = quantile(SOS_50, 0.75),
+            SOS_p97.5 = quantile(SOS_50, 0.975))
+            #SOS_max = max(SOS_50)) 
+
 
 #get a summary of median start of spring for each species of tree for use in the NPN analysis
   nyc_sos_summary <- 
@@ -44,7 +66,7 @@ nyc_sos <- read_csv ("C:/Users/dsk273/Box/Katz lab/NYC/tree_pheno/tree_pheno_poi
 
 
 #start species loop
-for(focal_sp_i in 1:1){
+for(focal_sp_i in 2:2){
   
   #inputs for loop
   focal_genus <- npn_species_to_analyze$genus[focal_sp_i] #focal_genus <-"Quercus"
@@ -58,7 +80,10 @@ for(focal_sp_i in 1:1){
     filter(R2 > 0.8 & dbh > 3.9) %>% 
     filter(n_SOS_50_7day > 0) %>% #removing trees that didn't have at least one image within a week of sos50
     mutate(leaf_mean = SOS_50,  #to match the naming of the NPN model
-           latitude = Lat)
+           latitude = Lat) %>% 
+    left_join(., nyc_sos_percentile_sp_yr) %>%  #filtering to the central 95% of the distribution to remove outliers
+    filter(SOS_50 > SOS_p2.5,
+           SOS_50 < SOS_p97.5)
   
   ### add in temperature data
     #create empty list to hold temperature data
@@ -158,8 +183,37 @@ for(focal_sp_i in 1:1){
         arrange(Point_ID, Year, difup_num) %>% 
         mutate(dif_lf_day = round(fit) + difup_num,
                doy = leaf_mean + dif_lf_day) %>% 
-        select(Point_ID, species, Year, doy, leaf_mean, dif_lf_day, prop_in_day) 
+        select(Point_ID, species, Year, doy, leaf_mean, dif_lf_day, prop_in_day) %>% 
+        mutate(obs_date = as.Date(doy - 1, paste0(Year, "-01-01")))
       
+      #normalizing by annual totals
+      flow_dif_preds_nyc_v3_annual_sum <-
+        flow_dif_preds_nyc_v3 %>% 
+        group_by(Year) %>% 
+        summarize(sum_year = sum(prop_in_day)) 
+
+      flow_dif_preds_nyc_v3 <-
+        left_join(flow_dif_preds_nyc_v3, flow_dif_preds_nyc_v3_annual_sum) %>% 
+        mutate(prop_in_day_n = prop_in_day/sum_year)
+      
+      
+      
+      #creating a version that has zeros so I can sum up rasters and distinguish zeros and NAs
+      flow_dif_preds_nyc_v3_all_zeros <- flow_dif_preds_nyc_v3 %>% ungroup() %>% 
+        expand(Point_ID, species, Year, doy) 
+        # mutate(prop_in_day = 0,
+        #        prop_in_day_n = 0)
+
+      flow_dif_preds_nyc_v3_with_0s <-       
+        flow_dif_preds_nyc_v3 %>% ungroup() %>% 
+        right_join(., flow_dif_preds_nyc_v3_all_zeros) %>% 
+        arrange(Point_ID, Year, doy) %>% 
+        mutate(prop_in_day = case_when(is.na(prop_in_day) ~ 0, .default = prop_in_day),
+               prop_in_day_n = case_when(is.na(prop_in_day_n) ~ 0, .default = prop_in_day_n))
+      
+
+      
+### summary figures for predicted flowering for each season #####################################      
       # filter(flow_dif_preds_nyc_v3) %>% 
       #   ggplot(aes(x = dif_lf_day, y = prop_in_day, color = as.factor(Point_ID))) + geom_line() + geom_point()+ theme_bw() + facet_wrap(~Year)
       # 
@@ -175,14 +229,88 @@ for(focal_sp_i in 1:1){
       
      flow_dif_preds_nyc_v3 %>% 
         group_by(doy, Year) %>% 
-        summarize(sum_day = sum(prop_in_day)) %>% 
-        ggplot(aes(x = doy, y = sum_day)) + geom_point() + facet_wrap(~Year) + theme_bw() + coord_cartesian(xlim = c(50, 200))
+        summarize(sum_day = sum(prop_in_day_n)) %>% 
+        ggplot(aes(x = (doy + mdy("01-01-2024")), y = sum_day, color = as.factor(Year))) + geom_line() + theme_bw() + 
+       scale_x_date(name = "date") + ylab("predicted peak flowering (proportion)") + scale_color_viridis_d(name = "Year")
+
+     
+### maps of flowering of individual trees for each species on each day ##########################
+  #load in nyc boundary polygon
+   nyc_boundary <- st_read( "C:/Users/dsk273/Box/Katz lab/NYC/nyc_boundary_polygon/nybb.shp") %>% 
+                  st_union() #combine the different boroughs
+   nyc_boundary_box <- st_as_sf(st_as_sfc(st_bbox(nyc_boundary)), crs= crs(nyc_boundary))
+   nyc_boundary_invert <- st_difference(nyc_boundary_box, nyc_boundary)
+
+  
+  #convert dataframe to sf
+    flow_dif_preds_nyc_v3_sf <-  nyc_sos_focal_sp %>% 
+     dplyr::select(Point_ID, species, Lon, Lat) %>% 
+     distinct() %>% #remove same tree from multiple years
+     left_join(flow_dif_preds_nyc_v3_with_0s, .) %>% 
+     st_as_sf(coords = c( "Lon", "Lat"), crs = 4326) %>% 
+       mutate(perc_in_day = prop_in_day * 100)
+  
+#create a map of peak flowering for each day
+  #create a directory to save individual frames to
+      output_directory <- paste0("C:/Users/dsk273/Box/Katz lab/NYC/tree_pheno/flowering_pred_maps/",
+                                focal_genus,"/", focal_species, "/")
+ 
+  #start loop for creating frames
+  for(focal_year_i in 2017:2024){
+    
+    #create a directory if it doesn't exist yet
+    output_directory_year <- paste0(output_directory, "year_", focal_year_i)
+    if(!dir.exists(output_directory_year)){dir.create(path = output_directory_year, recursive = TRUE)}
+    
+     #start the day loop
+    for(focal_doy_i in 100:140){
+  
+     flow_dif_preds_nyc_v3_sf_frame <- flow_dif_preds_nyc_v3_sf %>% 
+     filter(Year == focal_year_i) %>% 
+     filter(doy == focal_doy_i) 
+     
+     frame_focal_date <- as.Date(focal_doy_i - 1, paste0(focal_year_i, "-01-01"))
+   
+     # aggregate daily flowering points to raster
+     flow_dif_preds_nyc_v3_terra <- terra::vect(flow_dif_preds_nyc_v3_sf_frame)
+     nyc_grid <- rast(ext(flow_dif_preds_nyc_v3_terra), resolution = 0.03, crs = crs(flow_dif_preds_nyc_v3_terra))
+     flow_rast <- terra::rasterize(flow_dif_preds_nyc_v3_terra, nyc_grid, field = "perc_in_day", fun = "mean", background = NA)
+
+     #create map
+      map_frame <- ggplot() + ggthemes::theme_few() +   
+        theme(panel.background = element_rect(fill='gray94', color = NA)) + 
+        
+          geom_spatraster(data = flow_rast) + 
+           scale_fill_gradientn(colors = c("white","khaki2","gold", "red", "red3"), limits=c(0, 10), 
+                                na.value = NA, name = "peak flowering (%)") +
+            
+          geom_sf(data = nyc_boundary_invert, fill = "gray94", color = "gray94") +  #
+          geom_sf(data = nyc_boundary, fill = NA, color = "black") +
+          
+          geom_sf(data = flow_dif_preds_nyc_v3_sf_frame, aes(color = perc_in_day), size = 0.5, alpha = 0.9) + 
+              scale_color_gradientn(colors = c("white","khaki2","gold", "red", "red3"), limits=c(0, 10), 
+                                    na.value = NA, name = "peak flowering (%)", guide="none") +
       
+           annotation_scale(location = "br") +  # annotation_north_arrow(location = "tl") 
+           ggtitle(frame_focal_date)
+      
+      map_frame_title <- paste0(output_directory_year,
+                               "/map_frame_", focal_genus,"_", focal_species, "_", focal_year_i, "_", focal_doy_i,".png")
+      ggsave(filename = map_frame_title, plot = map_frame, units = "px", width = 3000, height = 2000)
+
+  } ## end loop for creating map of that day 
+    
+    ### create gifs from the individual frames ==================
+    #list of png files in directory
+    gif_file_list <- list.files(path = output_directory_year, full.names = TRUE)
+    gifski(gif_file_list, gif_file = paste0(output_directory, "animation_", focal_year_i,".gif"), delay = 0.5)
+    
+    
+  } ## end loop for year
+
+
   
-  ### aggregating daily flowering to pixel level
-  
-  
-}
+} ### end species loop
 
 
 
